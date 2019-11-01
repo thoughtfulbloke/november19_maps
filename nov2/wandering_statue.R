eddbf2e576d3b9e7684b800826c7fe9076ba292c
David Hood <david@thoughtful.net.nz>
2019-11-02 12:43:36 +1300

    Add files via upload

---
+++ nov2/wandering_statue.R
@@ -0,0 +1,16 @@
+# solid earth tide
+# data from using the DOS exe 'solid' from
+# https://geodesyworld.github.io/SOFTS/solid.htm
+# using robbie bruns statue, Dunedin, 2019-11-2 as input
+
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+
+nov2rob <- read.table("solid.txt", skip=2)
+names(nov2rob) <- c("second","m_north", "m_east", "m_up")
+nov2rob %>% mutate(to_north=lead(m_north), to_east=lead(m_east)) %>%
+ggplot(aes(x=m_east,y=m_north, colour=m_up, xend=to_east, yend=to_north)) + 
+  geom_segment(size=0.3) + scale_color_viridis_c("metres up") + coord_equal() + theme_tufte() +
+  ggtitle("Map of the 60 second solid earth tide generated location changes of Robert Burns statue, Dunedin, New Zealand for the day of 2nd November 2019 (UTC)") + xlab("position East (metres)") +
+  ylab("position North (metres)")
